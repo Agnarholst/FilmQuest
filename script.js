359e46af0d7e94ab06b546b98b952c9a2c2772a0
Agnar Holst <agnarholst@gmail.com>
2024-03-19 10:30:13 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,11 @@
+//------------------------CSV-BEHANDLING--------------------------------->
+
+// Her skal jeg laste inn watchlist.csv-filen og gjøre slik at det mulig å
+// hente ønsket informasjon fra de fire kategoriene:
+// Date,Name,Year,Letterboxd URI.
+
+//------------------------CSV-BEHANDLING---------------------------------|
+
+let test = document.getElementById("filmTitle");
+
+test.innerHTML = "Lord of the Rings: The Fellowship of the Ring"
