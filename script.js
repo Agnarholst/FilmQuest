a0ffe57cda8ad481b68e22914ee8eba6b0122d86
Agnar Holst <agnarholst@gmail.com>
2024-03-21 00:05:39 +0100

    Added a json-testfile with only three movies for testing. Added a randomNumber function that gives a new film title each time the page is refreshed.

--- script.js
+++ script.js
@@ -6,17 +6,33 @@
 
 //------------------------CSV-BEHANDLING---------------------------------|
 
-import Papa from 'papaparse';
+// import Papa from 'papaparse';
+// const file = new File("/letterboxd_data/watchlist.csv")
+// Papa.parse(file, {
+//     complete: function (results) {
+//         console.log("Finished: ", results.data)
+//     }
+// });
+
+let data = '{ "films" : [' +
+    '{ "Date": "2018-04-30", "Name": "Isle of Dogs", "Year": 2018, "Letterboxd URI": "https://boxd.it/dZrW" },' +
+    '{ "Date": "2018-04-30", "Name": "You Were Never Really Here", "Year": 2017, "Letterboxd URI": "https://boxd.it/dWOS" },' +
+    '{ "Date": "2018-05-03", "Name": "Ralph Breaks the Internet", "Year": 2018, "Letterboxd URI": "https://boxd.it/edAy" } ]}';
+
+let obj = JSON.parse(data);
+
+let watchList = obj.films;
+
+let randomFilm = watchList[randomNum()]
+let filmTitle = document.getElementById("filmTitle");
+
+filmTitle.innerHTML = randomFilm.Name
 
 
-const file = new File("/letterboxd_data/watchlist.csv")
+// Returns random number based on length of film list 
+function randomNum() {
+    const listLength = watchList.length
+    let randomizer = Math.ceil(Math.random() * listLength) - 1
+    return randomizer
+}
 
-Papa.parse(file, {
-    complete: function (results) {
-        console.log("Finished: ", results.data)
-    }
-});
-
-let test = document.getElementById("filmTitle");
-
-test.innerHTML = "Lord of the Rings: The Fellowship of the Ring"
